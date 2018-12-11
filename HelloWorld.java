15e56cab941bea62fd1827d9814999b26df83626
Jacob-Dunbar-CS <j.dunbar3@share.epsb.ca>
2018-12-11 12:05:22 -0700

    added template

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,31 @@
+/**<h1>Project Title:HelloWorld</h1>
+*<b>Purpose of Project:</b><br>
+*<p>Testing Pure Java Coding</p>
+*<b>How to start the project instructions:</b><br>
+*<p></p>
+*<b>User Instructions:<b/><br>
+*<p></P>
+*<br>
+*@author Jacob Dunbar
+*@version 1
+*@since 2018-12-11
+*/
+
 public class HelloWorld
 {
+	/**<b>Class Description: Necessary Class</b><br>
+	*<p></p>
+	*@param
+	*@return
+	*@throws
+	*/
+
+	/**<b>Method Description: Main, Driver</b><br>
+	*<p></p>
+	*@param args unused
+	*@return
+	*@throws
+	*/
 	public static void main (String [] args)
 	{
 		System.out.println("HelloWorld");
