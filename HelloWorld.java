8cb43805c19985507311b6e13b838374fad7f6cc
Jacob-Dunbar-CS <j.dunbar3@share.epsb.ca>
2018-12-13 10:16:42 -0700

    prints number of runs

--- HelloWorld.java
+++ HelloWorld.java
@@ -29,7 +29,7 @@ public class HelloWorld
 	public static void main (String [] args)
 	{
 		for (int i = 0; i < 20; i++) {
-			System.out.println("Hello World");
+			System.out.println("Hello World " + i);
 			
 		}
 	}
